a137fe931a4f5f2319654567ba5cdb8a892f321e
AnayelyDeLaVega <adelavega1@student.rccd.edu>
2020-06-25 15:48:09 -0700

    Signed-off-by: AnayelyDeLaVega <adelavega1@student.rccd.edu>

---
+++ Hello World/main.cpp
@@ -0,0 +1,40 @@
+/*
+ * File:   main.cpp
+ * Author: Anayely De La Vega
+ * Created on June 25, 2020, 3:27 PM
+ * Purpose: Our First Program
+ *          Gaddis, Walters, and Muganda from the book Starting out with 
+ *          C++ Early Objects, 8th Edition
+ *          Hello World Program 
+ */
+
+//System Libraries
+#include <iostream> //I/O Library 
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes 
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+    
+    //Declare all variables for this function
+    
+    //Initialize all known variables 
+    
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives 
+    
+    //Display the Inputs/Outputs
+    cout<<"Hello World"<<endl;
+    
+    //Clean up the code, close files. deallocate memory, etc...
+    //Exit stage right 
+    return 0;
+}
+
